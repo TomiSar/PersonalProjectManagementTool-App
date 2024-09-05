da89e631c7179bba7aa1688b3f2b18351a04d862
Tomi Sarjamo <tomi.sarjamo@tietoevry.com>
2024-09-05 21:07:11 +0300

    Some cleaning and separating the ProjectBoard, Backlog and ProjectTask components.

--- frontend/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ frontend/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,7 +1,20 @@
 function ProjectTask() {
   return (
-    <div>
-      <h1>Project Task</h1>
+    <div className='card mb-1 bg-light'>
+      <div className='card-header text-primary'>
+        ID: projectSequence -- Priority: priorityString
+      </div>
+      <div className='card-body bg-light'>
+        <h5 className='card-title'>project_task.summary</h5>
+        <p className='card-text text-truncate '>
+          project_task.acceptanceCriteria
+        </p>
+        <a href='' className='btn btn-primary'>
+          View / Update
+        </a>
+
+        <button className='btn btn-danger m-4'>Delete</button>
+      </div>
     </div>
   );
 }
